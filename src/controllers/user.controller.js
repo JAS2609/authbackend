75b22bff716baf6e6d28e8fe4767833fc9fbe990
Jas <jas72609@gmail.com>
2025-11-13 21:57:30 +0530

    fix export

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -188,7 +188,7 @@ const getCurrentUser=asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,req.user,"Current User Details"));
 
 })
-export const getMe = async (req, res) => {
+ const getMe = async (req, res) => {
   try {
     const userId = req.user.id; 
     const user = await User.findById(userId).select("id fullname username email role createdAt");
