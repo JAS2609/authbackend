ce23908073f0de94eb678314dcac7e39e959a9d2
Jas <jas72609@gmail.com>
2025-11-14 20:07:50 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 const app=express();
-
+app.set("trust proxy", 1);
 app.use(
     cors({
         origin:process.env.CORS_ORIGIN,
