d45e3182c2494e21c989db70639b68fa5becb627
Jas <jas72609@gmail.com>
2025-11-14 22:45:27 +0530

    fix controller error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -63,6 +63,49 @@ try {
 })
 
 
+const loginUser=asyncHandler(async(req,res)=>{
+  console.log("req.body:", req.body);
+
+  const{email,username,password}=req.body||{}
+   if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+       const user=await User.findOne({
+        $or:[{email},{username}]
+       }) 
+       if(!user){
+        throw new ApiError(404,"user not found")
+       }
+
+      const isPasswordValid= await user.isPasswordCorrect(password)
+      if(!isPasswordValid){
+        throw new ApiError(400,"invalid credentials")
+      }
+      const{accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
+
+      const loggedInUser=await User.findById(user._id)
+      .select("-password -refreshToken");
+      if(!loggedInUser){
+        throw new ApiError(500,"error in user login")
+      }
+      const isProd = process.env.NODE_ENV === "production";
+
+    const options = {
+      httpOnly: true,
+      secure: isProd,
+      sameSite: isProd ? "none" : "lax",
+      path: "/",
+    };
+
+      return res.status(200)
+      .cookie("accessToken",accessToken,options)
+      .cookie("refreshToken",refreshToken,options)
+      .json(new ApiResponse(
+        200,
+      { user: loggedInUser,accessToken,refreshToken},
+        "user logged in successfully"))
+
+})
 const loggedOutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
@@ -85,7 +128,6 @@ const loggedOutUser = asyncHandler(async (req, res) => {
     .clearCookie("refreshToken", cookieOptions)
     .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
-
 const refreshAccessToken=asyncHandler( async(req,res)=>{
   const incomingRefreshToken=req.cookies.refreshToken||req.body.refreshToken
   if(!incomingRefreshToken){
